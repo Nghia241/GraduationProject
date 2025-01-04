6039458abda7af616da5d0d108e2329cb100a91f
Nghia241 <Nghap1@gmail.com>
2025-01-04 23:39:58 +0700

    i18n_current_user_locale

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_02_191234) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_03_231421) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -62,15 +62,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_02_191234) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "ticket_types", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "type_name", null: false
-    t.text "description"
-    t.bigint "event_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_ticket_types_on_event_id"
-  end
-
   create_table "tickets", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "event_id"
@@ -101,7 +92,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_02_191234) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "ticket_types", "events"
   add_foreign_key "tickets", "events"
   add_foreign_key "tickets", "users"
   add_foreign_key "users", "system_roles"

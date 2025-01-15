00c697259a51a45ef8f04769b9837dbe308aef48
Nghia241 <Nghap1@gmail.com>
2025-01-16 03:51:23 +0700

    fix_error

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -27,7 +27,6 @@ class ProfilesController < ApplicationController
   def update_password
     @user = current_user
     if @user.update_with_password(password_params)
-      bypass_sign_in(@user) # Đăng nhập lại sau khi đổi mật khẩu
       flash[:notice] = "Cập nhật mật khẩu thành công!"
       redirect_to profile_path
     else
@@ -45,6 +44,6 @@ class ProfilesController < ApplicationController
 
   # Strong parameters cho mật khẩu
   def password_params
-    params.require(:user).permit(:current_password, :password, :password_confirmation)
+    params.permit(:current_password, :password, :password_confirmation)
   end
 end
